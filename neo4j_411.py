4497c5d2a95598760ef1c9a525836343d328ca61
pySaurus <39376268+pySaurus@users.noreply.github.com>
2020-11-30 23:41:14 -0600

    get_related_tickers added

--- neo4j_411.py
+++ neo4j_411.py
@@ -1,6 +1,6 @@
 # plz don't rename the file to 'neo4j.py' or this line wont work.
 from neo4j import GraphDatabase
-
+# import copy
 
 neo4j_uri  = 'bolt://localhost:7687'
 neo4j_user = 'neo4j'
@@ -60,6 +60,17 @@ def neo4j_insert_article(title, ticker):
 def neo4j_insert_relationship(title, ticker):
     print("neo4j_insert_relationship - start")
     with driver.session() as session:
+        result = session.run("MATCH (a:Article), (t:Ticker), (a)-[r:written_about]->(t) "
+                             "WHERE a.title=$title AND t.ticker=$ticker "
+                             "RETURN r ",
+                              ticker=ticker, title=title)
+        if result.peek():
+            print("neo4j_insert_relationship - relation already in neo4j")
+            session.close()
+            return
+        session.close()
+
+    with driver.session() as session:
         result = session.run("MATCH (a:Article), (t:Ticker) "
                              "WHERE a.title=$title AND t.ticker=$ticker "
                              "CREATE (a)-[:written_about]->(t) ",
@@ -75,9 +86,31 @@ def neo4j_get_high_impact_articles():
 
     with driver.session() as session:
         result = session.run("MATCH (a:Article)-[:written_about]->(t1:Ticker), (a:Article)-[:written_about]->(t2:Ticker) "
-                             "RETURN a", title=title)
+                             "RETURN a")
         print("neo4j_get_high_impact_articles - returning articles")
         return result
+
+#
+#
+#
+def neo4j_get_related_tickers():
+    print("neo4j_get_related_tickers - start")
+    with driver.session() as session:
+        result = session.run("MATCH (a:Article)-[:written_about]->(t1:Ticker) "
+                             "MATCH (a:Article)-[:written_about]->(t2:Ticker) "
+                             "WHERE t1.ticker <> t2.ticker "
+                             "RETURN DISTINCT t1.ticker, t2.ticker")
+        print("neo4j_get_related_tickers - query success, processing data")
+        ret = []
+        for record in result:
+            # print(record.values())
+            new_set = set(record.values())
+            if new_set not in ret:
+                ret.append(new_set)
+        info = result.consume()
+        print("neo4j_get_related_tickers - returning")
+        return ret
+
 
 # When I insert a ticker into sql, check if that ticker exists in neo4j
 #       If the ticker does not exist, insert it
@@ -94,17 +127,27 @@ def neo4j_get_high_impact_articles():
 
 if __name__ == "__main__":
 
-    title = "Apple shares good"
+    # title = "Apple shares good"
+    title = "Apple and Google shares good"
+
     ticker = "AAPL"
+    ticker = "GOOGL"
     # ticker = "nothing"
     # ret = neo4j_get_high_impact_articles()
     # print(ret)
     # print(ret.peek())
     # for r in ret:
     #     print(r)
-    neo4j_insert_article(title, ticker)
+    neo4j_insert_ticker(ticker="AAPL")
+    neo4j_insert_ticker(ticker="GOOGL")
+    neo4j_insert_article(title="Apple shares good", ticker="AAPL")
+    neo4j_insert_article(title="Apple and Google shares good", ticker="AAPL")
+    neo4j_insert_article(title="Apple and Google shares good", ticker="GOOGL")
+    # ret = neo4j_get_related_tickers()
+    # print(ret.peek())
+    # for r in ret:
+        # print(r)
     # neo4j_insert_ticker(ticker)
-
     # driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_pass))
     # session = driver.session()
     # result = session.run("CREATE (a:Article) "
