77a56a863ad23c998fc7cb05f84b3a4f1d1f9547
sam <satac2@illinois.edu>
2020-11-30 20:02:01 -0800

    reverted

--- news_api.py
+++ news_api.py
@@ -2,7 +2,6 @@ import requests
 import json
 import nltk
 
-from datetime import datetime
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import subjectivity, opinion_lexicon
 from nltk.sentiment import SentimentAnalyzer
@@ -23,73 +22,59 @@ for word in opinion_lexicon.negative():
 
 def getData(ticker, startDate, endDate):
     print("Querying NewsApi Endpoint for Data")
-    
+
+    url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI"
+    params = {"q": ticker,
+              "toPublishedDate": endDate,
+              "fromPublishedDate": startDate,
+              "pageSize": 50,
+              "pageNumber": 1,
+              "autoCorrect": True}
+    headers = {
+        'x-rapidapi-key': "d38bfb4a67mshe6eb67b553fd151p1c24e5jsn46e04dcf5498",
+        'x-rapidapi-host': "contextualwebsearch-websearch-v1.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=params)
+    d = response.json()
+    # print(d)
+
+    all_articles_from_api = d['value']
+    # print("-------------------------------------------------------------------")
+    progress = 0.0
     all_articles = []
-    old_date = endDate 
-    last_datePublished = endDate
-    while(last_datePublished > startDate):
-        url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI"
-        params = {"q": ticker,
-                "toPublishedDate": last_datePublished,
-                "fromPublishedDate": startDate,
-                "pageSize": 50,
-                "pageNumber": 1,
-                "autoCorrect": True}
-        headers = {
-            'x-rapidapi-key': "82918fd9abmsh35935d65203d30fp108b62jsnc80bbaba5de6",
-            'x-rapidapi-host': "contextualwebsearch-websearch-v1.p.rapidapi.com"
-        }
-        response = requests.request("GET", url, headers=headers, params=params)
-        d = response.json()
-        # print(d)
+    for article in all_articles_from_api:
+        title = article['title']
+        datePublished = article['datePublished']
+        link = article['url']
+        text = article['body']
+        id = article['id']
+        try:
+            sentiment = evaluate_sentence(text)
+        except Exception as e:
+            print(e)
+            print("You dont have the opinion_lexicon downloaded, so I'll just do that for you.")
+            print(" -Danny")
+            nltk.download('opinion_lexicon')
+            sentiment = evaluate_sentence(text)
 
-        all_articles_from_api = d['value']
-
-        # print("-------------------------------------------------------------------")
-        progress = 0.0
-
-
-        for article in all_articles_from_api:
-            title = article['title']
-            datePublished = article['datePublished']
-            link = article['url']
-            text = article['body']
-            id = article['id']
-            try:
-                sentiment = evaluate_sentence(text)
-            except Exception as e:
-                print(e)
-                print("You dont have the opinion_lexicon downloaded, so I'll just do that for you.")
-                print(" -Danny")
-                nltk.download('opinion_lexicon')
-                sentiment = evaluate_sentence(text)
-
-            # before we insert as a row, replace all of the double quote apostophes in title and text as single quote
-            title = title.replace("\"", "\'")
-            title = title.replace("`", "\'")
-            text = text.replace("\"", "\'")
-            text = text.replace("`", "\'")
-            
-            to_table = {
-                'title': title,
-                'link': link,
-                'text': text,
-                'articleDate': datePublished,
-                'sentiment': sentiment,
-                'articleID': id
-            }
-            all_articles.append(to_table)
-            progress += 1
-            print(datePublished[0:19])
-            datetime_object = datetime.strptime(datePublished[0:19], '%Y-%m-%dT%H:%M:%S')
-            last_datePublished = datetime.strftime(datetime_object.date(),'%Y-%m-%d')#datetime_object
+        # before we insert as a row, replace all of the double quote apostophes in title and text as single quote
+        title = title.replace("\"", "\'")
+        title = title.replace("`", "\'")
+        text = text.replace("\"", "\'")
+        text = text.replace("`", "\'")
         
-        if(last_datePublished == old_date):
-            break
-        old_date = last_datePublished
+        to_table = {
+            'title': title,
+            'link': link,
+            'text': text,
+            'articleDate': datePublished,
+            'sentiment': sentiment,
+            'articleID': id
+        }
+        all_articles.append(to_table)
 
+        progress += 1
         # print(100*progress/len(all_articles_from_api))
-
         # exit()
     print("Query NewsApi Endpoint Success")
     return all_articles
@@ -147,9 +132,9 @@ def evaluate_sentence(sentence):
 
 if __name__ == '__main__':
     ticker = "AAPL"
-    startDate = "2020-10-10"
-    endDate = "2020-10-30"
+    startDate = "2020-10-05"
+    endDate = "2020-10-15"
     data_return = getData(ticker, startDate, endDate)
-    #print(data_return)
-    #print('--------------------------------------------------------------------------------------------------------')
-    #print(data_return[0])
+    print(data_return)
+    print('--------------------------------------------------------------------------------------------------------')
+    print(data_return[0])
