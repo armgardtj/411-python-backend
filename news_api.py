a0d7d97a0bc4b67cfba08757ee847b2ebc14fb8d
sam <satac2@illinois.edu>
2020-11-30 19:10:37 -0800

    getting all the articles

--- news_api.py
+++ news_api.py
@@ -2,6 +2,7 @@ import requests
 import json
 import nltk
 
+from datetime import datetime
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import subjectivity, opinion_lexicon
 from nltk.sentiment import SentimentAnalyzer
@@ -22,58 +23,65 @@ for word in opinion_lexicon.negative():
 
 def getData(ticker, startDate, endDate):
     print("Querying NewsApi Endpoint for Data")
-
-    url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI"
-    params = {"q": ticker,
-              "toPublishedDate": endDate,
-              "fromPublishedDate": startDate,
-              "pageSize": 50,
-              "pageNumber": 1,
-              "autoCorrect": True}
-    headers = {
-        'x-rapidapi-key': "82918fd9abmsh35935d65203d30fp108b62jsnc80bbaba5de6",
-        'x-rapidapi-host': "contextualwebsearch-websearch-v1.p.rapidapi.com"
-    }
-    response = requests.request("GET", url, headers=headers, params=params)
-    d = response.json()
-    # print(d)
-
-    all_articles_from_api = d['value']
-    # print("-------------------------------------------------------------------")
-    progress = 0.0
+    
     all_articles = []
-    for article in all_articles_from_api:
-        title = article['title']
-        datePublished = article['datePublished']
-        link = article['url']
-        text = article['body']
-        id = article['id']
-        try:
-            sentiment = evaluate_sentence(text)
-        except Exception as e:
-            print(e)
-            print("You dont have the opinion_lexicon downloaded, so I'll just do that for you.")
-            print(" -Danny")
-            nltk.download('opinion_lexicon')
-            sentiment = evaluate_sentence(text)
-
-        # before we insert as a row, replace all of the double quote apostophes in title and text as single quote
-        title = title.replace("\"", "\'")
-        title = title.replace("`", "\'")
-        text = text.replace("\"", "\'")
-        text = text.replace("`", "\'")
-        
-        to_table = {
-            'title': title,
-            'link': link,
-            'text': text,
-            'articleDate': datePublished,
-            'sentiment': sentiment,
-            'articleID': id
+    last_datePublished = endDate
+    while(last_datePublished > startDate):
+        url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI"
+        params = {"q": ticker,
+                "toPublishedDate": last_datePublished,
+                "fromPublishedDate": startDate,
+                "pageSize": 50,
+                "pageNumber": 1,
+                "autoCorrect": True}
+        headers = {
+            'x-rapidapi-key': "82918fd9abmsh35935d65203d30fp108b62jsnc80bbaba5de6",
+            'x-rapidapi-host': "contextualwebsearch-websearch-v1.p.rapidapi.com"
         }
-        all_articles.append(to_table)
+        response = requests.request("GET", url, headers=headers, params=params)
+        d = response.json()
+        # print(d)
 
-        progress += 1
+        all_articles_from_api = d['value']
+
+        # print("-------------------------------------------------------------------")
+        progress = 0.0
+
+        
+        for article in all_articles_from_api:
+            title = article['title']
+            datePublished = article['datePublished']
+            link = article['url']
+            text = article['body']
+            id = article['id']
+            try:
+                sentiment = evaluate_sentence(text)
+            except Exception as e:
+                print(e)
+                print("You dont have the opinion_lexicon downloaded, so I'll just do that for you.")
+                print(" -Danny")
+                nltk.download('opinion_lexicon')
+                sentiment = evaluate_sentence(text)
+
+            # before we insert as a row, replace all of the double quote apostophes in title and text as single quote
+            title = title.replace("\"", "\'")
+            title = title.replace("`", "\'")
+            text = text.replace("\"", "\'")
+            text = text.replace("`", "\'")
+            
+            to_table = {
+                'title': title,
+                'link': link,
+                'text': text,
+                'articleDate': datePublished,
+                'sentiment': sentiment,
+                'articleID': id
+            }
+            all_articles.append(to_table)
+
+            progress += 1
+            datetime_object = datetime.strptime(datePublished, '%Y-%m-%dT%H:%M:%S')
+            last_datePublished = datetime.strftime(datetime_object.date(),'%Y-%m-%d')#datetime_object
         # print(100*progress/len(all_articles_from_api))
         # exit()
     print("Query NewsApi Endpoint Success")
@@ -132,9 +140,9 @@ def evaluate_sentence(sentence):
 
 if __name__ == '__main__':
     ticker = "AAPL"
-    startDate = "2020-10-05"
-    endDate = "2020-10-15"
+    startDate = "2020-10-10"
+    endDate = "2020-10-30"
     data_return = getData(ticker, startDate, endDate)
-    print(data_return)
-    print('--------------------------------------------------------------------------------------------------------')
-    print(data_return[0])
+    #print(data_return)
+    #print('--------------------------------------------------------------------------------------------------------')
+    #print(data_return[0])
